568f56353824e0dba84acef2896ee73f48b269da
Michael Großklaus <mail@mgrossklaus.de>
2023-10-16 20:45:49 +0200

    Move all translations to en.yml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def active_dashboard_menu_state(page)
     return ' aria-current="true"'.html_safe if @active_dashboard_menu == page
   end
+
+  def abc
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  end
 end
