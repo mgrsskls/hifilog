b1bb34c103c90a56654e6821596a413afc0b5a2e
Michael Großklaus <mail@mgrossklaus.de>
2023-10-16 20:19:02 +0200

    Highlight brands owned by user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :user_has_product?
+    helper_method :user_has_brand?
 
     def current_user=(user)
         @current_user = user
@@ -13,5 +14,10 @@ class ApplicationController < ActionController::Base
     def user_has_product?(product)
         return if !product
         user_signed_in? && current_user.products.include?(product)
+    end
+
+    def user_has_brand?(brand)
+        return if !brand
+        user_signed_in? && current_user.products.select { |p| p.brand_id == brand.id }.any?
     end
 end
