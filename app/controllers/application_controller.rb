5b3382798aafda58c2c2348326a43f534a716b41
Michael Großklaus <mail@mgrossklaus.de>
2024-01-20 11:09:30 -0800

    Fixes username not saved when entered during registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,6 +53,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
         devise_parameter_sanitizer.permit(:account_update, keys: [:profile_visibility, :user_name])
     end
 
