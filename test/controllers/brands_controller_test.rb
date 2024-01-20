87579a5ab2ac245671270318c0dbd65c3ce7b460
Michael Großklaus <mail@mgrossklaus.de>
2024-01-20 12:11:45 -0800

    Adds some basic tests

--- test/controllers/brands_controller_test.rb
+++ test/controllers/brands_controller_test.rb
@@ -2,7 +2,12 @@ require "test_helper"
 
 class BrandsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get brands_index_url
+    get brands_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get brand_url(id: "feliks-audio")
     assert_response :success
   end
 end
