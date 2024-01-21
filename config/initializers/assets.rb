56dbbad025931bfc69e8cad540e42ca727c387f7
Michael Großklaus <mail@mgrossklaus.de>
2024-01-20 16:42:07 -0800

    Use PostCSS

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,12 +9,12 @@ Rails.application.config.assets.version = "1.0"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w(
-  accordion_tabs.css
-  dashboard.css
-  new_product.css
-  product.css
-  setups.css
-  search_results.css
-  user_form.css
-)
+# Rails.application.config.assets.precompile += %w(
+#   accordion_tabs.css
+#   dashboard.css
+#   new_product.css
+#   product.css
+#   setups.css
+#   search_results.css
+#   user_form.css
+# )
