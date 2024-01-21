0e2748d9473fe86c50b8ffef60f6391bdbfc4e14
Michael Großklaus <mail@mgrossklaus.de>
2024-01-20 22:02:35 -0800

    Add confirmable to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_20_213636) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_21_053401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -133,6 +133,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_20_213636) do
     t.integer "profile_visibility", default: 0
     t.string "random_username"
     t.string "user_name"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
