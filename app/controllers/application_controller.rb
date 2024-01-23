2d2964e72aa3b321c7d215eafb58b9ebe3dead4f
Michael Großklaus <mail@mgrossklaus.de>
2024-01-23 11:20:02 -0800

    Use .find_each instead of .all.each (perf)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -64,21 +64,21 @@ class ApplicationController < ActionController::Base
   def sitemap
     map = XmlSitemap::Map.new('hifilog.com') do |m|
       m.add brands_path
-      Brand.all.each do |brand|
+      Brand.find_each do |brand|
         m.add brand_path(brand)
       end
 
       m.add categories_path
-      Category.all.each do |category|
+      Category.find_each do |category|
         m.add category_path(category)
       end
 
       m.add products_path
-      Product.all.each do |product|
+      Product.find_each do |product|
         m.add brand_product_path(id: product.friendly_id, brand_id: product.brand.friendly_id), updated: product.updated_at
       end
 
-      SubCategory.all.each do |sub_category|
+      SubCategory.find_each do |sub_category|
         m.add category_sub_category_path(id: sub_category.friendly_id, category_id: sub_category.category.friendly_id)
       end
     end
