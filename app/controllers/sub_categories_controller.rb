f67c4f0b5f7f91e0029abb0dd7d3a04e82ffa0a3
Michael Großklaus <mail@mgrossklaus.de>
2024-01-24 05:22:58 -0800

    Linting

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -18,8 +18,9 @@ class SubCategoriesController < ApplicationController
     if params[:letter]
       add_breadcrumb @sub_category.name, category_sub_category_path(@sub_category)
       add_breadcrumb params[:letter].upcase
-      @products = @sub_category.products.includes([:brand]).where('name ILIKE :prefix',
-                                               prefix: "#{params[:letter]}%").order('LOWER(name)').page(params[:page])
+      @products = @sub_category.products.includes([:brand])
+                               .where('name ILIKE :prefix', prefix: "#{params[:letter]}%")
+                               .order('LOWER(name)').page(params[:page])
     else
       add_breadcrumb @sub_category.name
       @products = @sub_category.products.includes([:brand]).order('LOWER(name)').page(params[:page])
