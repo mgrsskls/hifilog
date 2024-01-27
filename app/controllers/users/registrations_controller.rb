63292649a34b07fb3820a99322368a463acca1d9
Michael Großklaus <mail@mgrossklaus.de>
2024-01-26 21:00:42 -0800

    Fix error when deleting account

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -37,10 +37,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    Bookmark.where(user_id: current_user.id).find_each(&:destroy!)
+
+    super
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
