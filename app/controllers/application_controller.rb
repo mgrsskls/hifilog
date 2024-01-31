72922725737b130087ddc6cd6f1867c1909ddfc5
Michael Großklaus <mail@mgrossklaus.de>
2024-01-30 16:19:29 -0800

    Move sitemap into separate controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,34 +62,6 @@ class ApplicationController < ActionController::Base
     render 'not_found', status: :not_found
   end
 
-  def sitemap
-    map = XmlSitemap::Map.new('www.hifilog.com', secure: true) do |m|
-      m.add brands_path
-      Brand.find_each do |brand|
-        m.add brand_path(brand)
-      end
-
-      m.add categories_path
-      Category.find_each do |category|
-        m.add category_path(category)
-      end
-
-      m.add products_path
-      Product.includes([:brand]).find_each do |product|
-        m.add brand_product_path(
-          id: product.friendly_id,
-          brand_id: product.brand.friendly_id
-        ), updated: product.updated_at
-      end
-
-      SubCategory.includes([:category]).find_each do |sub_category|
-        m.add category_sub_category_path(id: sub_category.friendly_id, category_id: sub_category.category.friendly_id)
-      end
-    end
-
-    render xml: map.render
-  end
-
   protected
 
   def configure_permitted_parameters
