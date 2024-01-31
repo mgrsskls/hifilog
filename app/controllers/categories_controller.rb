dd8d1bf4b4e9276838f736fb6325e930a22e006a
Michael Großklaus <mail@mgrossklaus.de>
2024-01-30 16:46:09 -0800

    Order categories via db, add model method for ordered categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
     @active_menu = :categories
     @page_title = I18n.t('headings.categories')
 
-    @categories = Category.all.sort_by { |c| c[:name].downcase }
+    @categories = Category.ordered
 
     sub_categories = if params[:id].present?
                        SubCategory.where(category_id: params[:id].to_i).includes([:category])
@@ -19,7 +19,7 @@ class CategoriesController < ApplicationController
     @active_menu = :categories
 
     @category = Category.friendly.find(params[:id])
-    @categories = Category.all.sort_by { |c| c[:name].downcase }
+    @categories = Category.ordered
     @sub_categories = @category.sub_categories.sort_by { |c| c[:name].downcase }
 
     add_breadcrumb @category.name, category_path(@category)
