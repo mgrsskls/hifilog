a753f9a856f569fef3226963d1019009ea0bd44c
Michael Großklaus <mail@mgrossklaus.de>
2024-01-31 17:13:10 -0800

    Memoize records count and newest products and brands

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,9 +1,6 @@
 class FeedController < ApplicationController
   def rss
-    products = Product.includes([:brand]).order(created_at: :desc).limit(10)
-    brands = Brand.order(created_at: :desc).limit(10)
-
-    @all = (products + brands).sort_by(&:created_at)
+    @all = (newest_products + newest_brands).sort_by(&:created_at).reverse!
 
     respond_to do |format|
       format.rss { render layout: false }
