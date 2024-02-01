8aaff03f43a7dfcb1f4a960d6b08011e0d85b99c
Michael Großklaus <mail@mgrossklaus.de>
2024-01-31 20:29:14 -0800

    Make sure brands index on name is case-insensitive

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_031354) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_01_042250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_031354) do
     t.datetime "updated_at", null: false
     t.boolean "discontinued"
     t.string "slug"
-    t.index ["name"], name: "index_brands_on_name", unique: true
+    t.index "lower((name)::text)", name: "index_brands_on_name", unique: true
     t.index ["slug"], name: "index_brands_on_slug", unique: true
   end
 
