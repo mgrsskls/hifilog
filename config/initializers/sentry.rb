65ed27fb9a4066c8f00faa6fe469d479c0405c26
Michael Großklaus <mail@mgrossklaus.de>
2024-02-02 14:34:28 -0800

    Use sentry only for production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,8 @@
-Sentry.init do |config|
-  config.dsn = 'https://21e83dc22ecca6f76d65bfac64d5d7be@o4506621346512896.ingest.sentry.io/4506621346643968'
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.traces_sample_rate = 1.0
-  config.profiles_sample_rate = 1.0
+if Rails.env.production?
+  Sentry.init do |config|
+    config.dsn = ENV["SENTRY_DSN"]
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.traces_sample_rate = 1.0
+    config.profiles_sample_rate = 1.0
+  end
 end
