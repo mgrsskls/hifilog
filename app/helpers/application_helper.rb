46a87c62b6f868a7c42364c8350a09d55773870c
Michael Großklaus <mail@mgrossklaus.de>
2024-02-06 02:00:38 -0800

    Improve changelog output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -63,4 +63,11 @@ module ApplicationHelper
   def changelog(changes)
     PaperTrail::Serializers::YAML.load(changes)
   end
+
+  def country_name(country_code)
+    return nil if country_code.nil?
+
+    country = ISO3166::Country[country_code]
+    country.translations[I18n.locale.to_s] || country.common_name || country.iso_short_name
+  end
 end
