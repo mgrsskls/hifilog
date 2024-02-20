38e727ad9851c357ecd2d864e27b4d39e95c2d59
Michael Großklaus <mail@mgrossklaus.de>
2024-02-19 17:08:26 -0800

    Trying to fix image cdn

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,9 +1,9 @@
 if Rails.env.production?
   Aws.config[:credentials] = Aws::Credentials.new(
-    ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
-    ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
+    ENV['AWS_ACCESS_KEY_ID'],
+    ENV['AWS_SECRET_ACCESS_KEY']
   )
-  Aws.config[:region] = ENV['BUCKETEER_AWS_REGION']
+  Aws.config[:region] = ENV['AWS_REGION']
 
   S3 = Aws::S3::Client.new
 end
