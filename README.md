5240734aff94dc305f9c13e7537a3758c8bf8768
Michael Großklaus <mail@mgrossklaus.de>
2024-02-19 22:18:57 -0800

    Activate CSP again

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Setup
 
-### Setting up the pre-commit hook
-
 1. `cp .pre-commit .git/hooks/pre-commit`
+2. `touch .env`
+3. Add `CDN_HOST=http://127.0.0.1:3000` to `.env`
--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,22 +5,22 @@
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https, :unsafe_inline
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-# #
-# #   # Generate session nonces for permitted importmap and inline scripts
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src)
-# #
-# #   # Report violations without enforcing the policy.
-# #   # config.content_security_policy_report_only = true
-# end
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data, ENV['CDN_HOST']
+    policy.object_src  :none
+    policy.script_src  :self, :https, ENV['CDN_HOST']
+    policy.style_src   :self, :https, :unsafe_inline, ENV['CDN_HOST']
+    # Specify URI for violation reports
+    # policy.report_uri "/csp-violation-report-endpoint"
+  end
+#
+#   # Generate session nonces for permitted importmap and inline scripts
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w(script-src)
+#
+#   # Report violations without enforcing the policy.
+#   # config.content_security_policy_report_only = true
+end
