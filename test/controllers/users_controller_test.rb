d3fa23e93405b0427a45586e70938b3bef629f62
Michael Großklaus <mail@mgrossklaus.de>
2024-03-11 21:47:58 -0700

    Fix test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Metrics/BlockLength
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
@@ -17,16 +16,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get user_path(id: users(:logged_in_only).user_name)
     assert_response :redirect
     assert_redirected_to new_user_session_path(redirect: user_path(id: users(:logged_in_only).user_name))
-    get user_previous_products_path(user_id: users(:logged_in_only).user_name)
-    assert_response :redirect
-    assert_redirected_to new_user_session_path(
-      redirect: user_previous_products_path(user_id: users(:logged_in_only).user_name)
-    )
 
     # profile visible for logged in and logged out users
     get user_path(id: users(:visible).user_name)
-    assert_response :success
-    get user_previous_products_path(user_id: users(:visible).user_name)
     assert_response :success
 
     sign_in users(:visible)
@@ -35,21 +27,46 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get user_path(id: users(:hidden).user_name)
     assert_response :redirect
     assert_redirected_to root_path
-    get user_previous_products_path(user_id: users(:hidden).user_name)
-    assert_response :redirect
-    assert_redirected_to root_path
 
     # profile visible only for logged in users
     get user_path(id: users(:logged_in_only).user_name)
-    assert_response :success
-    get user_previous_products_path(user_id: users(:logged_in_only).user_name)
     assert_response :success
 
     # profile visible for logged in and logged out users
     get user_path(id: users(:visible).user_name)
     assert_response :success
+  end
+
+  test 'prev_owneds' do
+    # profile hidden
+    get user_previous_products_path(user_id: users(:hidden).user_name)
+    assert_response :redirect
+    assert_redirected_to root_path
+
+    # profile visible only for logged in users
+    get user_previous_products_path(user_id: users(:logged_in_only).user_name)
+    assert_response :redirect
+    assert_redirected_to new_user_session_path(
+      redirect: user_previous_products_path(user_id: users(:logged_in_only).user_name)
+    )
+
+    # profile visible for logged in and logged out users
+    get user_previous_products_path(user_id: users(:visible).user_name)
+    assert_response :success
+
+    sign_in users(:visible)
+
+    # profile hidden
+    get user_previous_products_path(user_id: users(:hidden).user_name)
+    assert_response :redirect
+    assert_redirected_to root_path
+
+    # profile visible only for logged in users
+    get user_previous_products_path(user_id: users(:logged_in_only).user_name)
+    assert_response :success
+
+    # profile visible for logged in and logged out users
     get user_previous_products_path(user_id: users(:visible).user_name)
     assert_response :success
   end
 end
-# rubocop:enable Metrics/BlockLength
