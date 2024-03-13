dea35ceb34068e59f080b29e1c2755f18fe2ec40
Michael Großklaus <mail@mgrossklaus.de>
2024-03-13 07:46:27 -0700

    Fix 500 when no search term is given

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,12 +6,12 @@ class SearchController < ApplicationController
   def results
     @page_title = I18n.t('search')
     @no_index = true
-    @query = params[:query].strip
-    query_split_up = @query.split
+    @query = params[:query].present? ? params[:query].strip : nil
 
-    if @query.length < MIN_CHARS
+    if @query.nil? || @query.length < MIN_CHARS
       flash.now[:alert] = I18n.t('search_results.alert.minimum_chars', min: MIN_CHARS)
     else
+      query_split_up = @query.split
       @products = get_products(query_split_up, 20)
       @brands = get_brands(query_split_up, 20)
     end
