3ed7927f4c699a5b842a1780be4404d2a8860e48
Michael Großklaus <mail@mgrossklaus.de>
2024-03-13 07:42:46 -0700

    Remove comment

--- run-axe.js
+++ run-axe.js
@@ -17,24 +17,6 @@ const PATHS = [
 	"/search?query=concept",
 ];
 
-// exec(
-// 	`node_modules/.bin/axe ${PATHS.map((path) => `${process.env.A11Y_HOST || "http://127.0.0.1:3000"}${path}`)} --exit --show-errors`,
-// 	(error, stdout, stderr) => {
-// 		if (error !== null) {
-// 			console.log("error");
-// 			console.error(`exec error: ${error}`);
-// 			process.exit(1);
-// 		} else if (stderr.length > 0) {
-// 			console.log("stderr");
-// 			console.error(`exec error: ${stderr}`);
-// 			process.exit(1);
-// 		} else {
-// 			console.log("success");
-// 			process.exit(0);
-// 		}
-// 	},
-// );
-
 const ls = spawn("node_modules/.bin/axe", [
 	PATHS.map(
 		(path) => `${process.env.A11Y_HOST || "http://127.0.0.1:3000"}${path}`,
