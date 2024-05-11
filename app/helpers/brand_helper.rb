a79106c70c058e8effab8dd3dcda1d67f715bb32
Michael Großklaus <mail@mgrossklaus.de>
2024-05-11 21:16:42 +0200

    Fix n+1 query

--- app/helpers/brand_helper.rb
+++ app/helpers/brand_helper.rb
@@ -1,7 +1,7 @@
 module BrandHelper
   def all_sub_categories(brand)
     (
-      brand.sub_categories +
+      brand.sub_categories.includes([:category]) +
       SubCategory.includes(:category).joins(:products).where(products: brand.products).order(:name).distinct
     ).uniq.sort_by(&:name)
   end
