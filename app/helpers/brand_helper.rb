ae15a1d160db6c6f56cd9d82ab449ce60eff1faf
Michael Großklaus <mail@mgrossklaus.de>
2024-05-12 09:53:45 +0200

    Combined query for all sub categories of a brand

--- app/helpers/brand_helper.rb
+++ app/helpers/brand_helper.rb
@@ -1,8 +1,20 @@
 module BrandHelper
   def all_sub_categories(brand)
-    (
-      brand.sub_categories.includes([:category]) +
-      SubCategory.includes(:category).joins(:products).where(products: brand.products).order(:name).distinct
-    ).uniq.sort_by(&:name)
+    SubCategory.find_by_sql(["
+      SELECT * FROM (
+        SELECT sub_categories.*
+        FROM sub_categories
+        INNER JOIN brands_sub_categories
+        ON sub_categories.id = brands_sub_categories.sub_category_id
+        WHERE brands_sub_categories.brand_id = ?
+        UNION
+        SELECT DISTINCT sub_categories.*
+        FROM sub_categories
+        INNER JOIN products_sub_categories
+        ON products_sub_categories.sub_category_id = sub_categories.id
+        INNER JOIN products ON products.id = products_sub_categories.product_id
+        WHERE products.id IN (SELECT products.id FROM products WHERE products.brand_id = ?)
+      ) AS sub_category ORDER BY sub_category.name
+    ", brand.id, brand.id])
   end
 end
