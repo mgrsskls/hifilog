4559ba008a594ba0af8cedf8c4e03491c7d5f8f0
Michael Großklaus <mail@mgrossklaus.de>
2024-08-24 10:33:30 +0200

    "Owned for" added to history

--- app/presenters/possession_presenter.rb
+++ app/presenters/possession_presenter.rb
@@ -1,5 +1,6 @@
 class PossessionPresenter < ItemPresenter
   include Rails.application.routes.url_helpers
+  include ActionView::Helpers::DateHelper
 
   def image_update_item
     @object
@@ -19,5 +20,16 @@ class PossessionPresenter < ItemPresenter
 
   def period_to
     @object.period_to
+  end
+
+  def owned_for
+    parsed_from = period_from.to_date if period_from.present?
+    parsed_to = period_to.to_date if period_to.present?
+
+    if prev_owned
+      distance_of_time_in_words(parsed_to, parsed_from) if parsed_to && parsed_from
+    elsif parsed_from
+      distance_of_time_in_words(Time.zone.today, parsed_from)
+    end
   end
 end
