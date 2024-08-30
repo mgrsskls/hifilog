cf6eb0d863d8fce2b9dd19168b17499b5eb3bfe0
Michael Großklaus <mail@mgrossklaus.de>
2024-08-30 22:41:39 +0200

    Try to avoid flashing of light/dark mode

--- app/assets/javascripts/theme_toggle.js
+++ app/assets/javascripts/theme_toggle.js
@@ -1,11 +1,7 @@
 customElements.define(
 	"theme-toggle",
 	class ThemeToggle extends HTMLElement {
-		#DEFAULT_THEMES = ["auto", "light", "dark"];
-		#DEFAULT_NAME = "theme";
-		#index;
 		#name;
-		#themes;
 		#currentTheme;
 		#defaultTheme;
 		#inputs;
@@ -15,40 +11,19 @@ customElements.define(
 		}
 
 		connectedCallback() {
-			this.#index = Array.from(
-				document.querySelectorAll("theme-toggle"),
-			).indexOf(this);
+			this.#inputs = Array.from(this.querySelectorAll('[type="radio"]'));
+			this.#name = this.#inputs[0].name;
+			this.#currentTheme = this.#getStoredTheme() || this.#getCurrentTheme();
+			this.#defaultTheme = this.#inputs[0].value;
 
-			window.requestAnimationFrame(() => {
-				this.#inputs = Array.from(this.querySelectorAll('[type="radio"]'));
-				if (this.#inputs.length > 0) {
-					this.#name = this.#inputs[0].name;
-					this.#themes = this.#inputs.map((option) => option.value);
-					this.#currentTheme =
-						this.#getStoredTheme() ||
-						this.#getCurrentTheme() ||
-						this.#themes[0];
-				} else {
-					this.#name =
-						this.getAttribute("name") ||
-						`${this.#DEFAULT_NAME}${this.#index > 0 ? `-${this.#index}` : ""}`;
-					this.#themes =
-						this.getAttribute("themes")?.split(" ") || this.#DEFAULT_THEMES;
-					this.#currentTheme = this.#getStoredTheme() || this.#themes[0];
-					this.#inputs = this.#addOptions();
-				}
+			this.#render();
 
-				this.#defaultTheme = this.#inputs[0].value;
+			this.#inputs.forEach((input) => {
+				input.addEventListener("change", ({ target }) => {
+					const { value } = target;
 
-				this.#render();
-
-				this.#inputs.forEach((input) => {
-					input.addEventListener("change", ({ target }) => {
-						const { value } = target;
-
-						this.#saveTheme(value);
-						this.#render();
-					});
+					this.#saveTheme(value);
+					this.#render();
 				});
 			});
 		}
@@ -76,6 +51,8 @@ customElements.define(
 		}
 
 		#render() {
+			document.documentElement.dataset.theme = this.#currentTheme;
+
 			const input = this.#inputs.find(
 				(input) => input.value === this.#currentTheme,
 			);
@@ -83,29 +60,6 @@ customElements.define(
 			if (input) {
 				input.checked = true;
 			}
-		}
-
-		#addOptions() {
-			return this.#themes.map((theme) => {
-				const div = document.createElement("div");
-				const input = document.createElement("input");
-				const label = document.createElement("label");
-				const id = `${"theme-toggle"}-${this.#index}-${theme}`;
-
-				input.type = "radio";
-				input.name = this.#name;
-				input.value = theme;
-				input.id = id;
-
-				label.setAttribute("for", id);
-				label.textContent = theme;
-
-				div.appendChild(input);
-				div.appendChild(label);
-				this.appendChild(div);
-
-				return input;
-			});
 		}
 	},
 );
