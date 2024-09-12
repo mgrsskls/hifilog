60a4ba4b742ccc48580a084415b5c07d673fa9ed
Michael Großklaus <mail@mgrossklaus.de>
2024-09-12 20:51:07 +0200

    Adds missing presence validator for :text in AppNews

--- app/models/app_news.rb
+++ app/models/app_news.rb
@@ -3,6 +3,8 @@ class AppNews < ApplicationRecord
 
   has_and_belongs_to_many :users
 
+  validates :text, presence: true
+
   def formatted_text
     # rubocop:disable Rails/OutputSafety
     markdown_to_html(text).html_safe
