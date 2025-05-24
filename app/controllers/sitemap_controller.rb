6f0b540fdbb41d8196c32745ae82bfa3dba7677a
Michael Großklaus <mail@mgrossklaus.de>
2025-05-24 08:23:30 +0200

    Adds more sites to sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,6 +1,16 @@
 class SitemapController < ApplicationController
   def xml
     map = XmlSitemap::Map.new('www.hifilog.com', secure: true) do |m|
+      m.add users_path
+      User.where(profile_visibility: 2).find_each do |user|
+        m.add user_path(user.user_name.downcase)
+      end
+
+      m.add changelog_path
+      m.add about_path
+      m.add calculators_root_path
+      m.add calculators_resistors_for_amplifier_to_headphone_adapter_path
+
       m.add brands_path
       Brand.find_each do |brand|
         m.add brand_path(brand)
