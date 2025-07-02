e891db0bff36f4bba1e3570bb157a9dabb9a4ea1
Michael Großklaus <mail@mgrossklaus.de>
2025-07-02 18:07:51 +0200

    wip

--- app/controllers/concerns/filter_params_builder.rb
+++ app/controllers/concerns/filter_params_builder.rb
@@ -8,6 +8,8 @@ module FilterParamsBuilder
     filters[:sub_category] = @sub_category if @sub_category.present?
 
     filters[:status] = params_hash[:status] if params_hash[:status].present? && STATUSES.include?(params_hash[:status])
+    filters[:query] = params_hash[:query] if params_hash[:query].present? && !params_hash[:query].strip.empty?
+    filters[:sort] = params_hash[:sort] if params_hash[:sort].present?
 
     if params_hash[:diy_kit].present? && %w[0 1].include?(params_hash[:diy_kit])
       filters[:diy_kit] = params_hash[:diy_kit]
@@ -17,11 +19,7 @@ module FilterParamsBuilder
       filters[:country] = params_hash[:country]
     end
 
-    filters[:query] = params_hash[:query] if params_hash[:query].present? && !params_hash[:query].strip.empty?
-
-    filters[:attr] = params_hash[:attr] if params_hash[:attr].present?
-
-    filters[:sort] = params_hash[:sort] if params_hash[:sort].present?
+    filters[:custom] = params_hash.except(:category, :sub_category, :letter, :status, :query, :sort, :diy_kit, :country)
 
     filters
   end
--- app/models/custom_attribute.rb
+++ app/models/custom_attribute.rb
@@ -1,5 +1,48 @@
 class CustomAttribute < ApplicationRecord
+  VALID_UNITS = %w[in cm lb kg db w ohm hz db_1w_1m db_283v_1m].freeze
+  VALID_INPUTS = %w[w h l min max].freeze
+
   has_and_belongs_to_many :sub_categories
+  enum :input_type, {
+    number: 'number',
+    option: 'option',
+    options: 'options',
+  }, suffix: true
+
+  validates :label, presence: true, uniqueness: true
+  validate :units_must_be_valid
+  validate :inputs_must_be_valid
+
+  before_validation do
+    self.units = units.compact_blank if units.is_a?(Array)
+    self.inputs = inputs.compact_blank if inputs.is_a?(Array)
+  end
+
+  before_save do
+    if options.present?
+      self.options = JSON.parse(options) if options.is_a?(String)
+    else
+      self.options = nil
+    end
+  end
+
+  def units_must_be_valid
+    return if units.blank?
+
+    cleaned_units = units.compact_blank
+    invalid = cleaned_units - VALID_UNITS
+
+    errors.add(:units, "contain invalid values: #{invalid.join(', ')}") if invalid.any?
+  end
+
+  def inputs_must_be_valid
+    return if inputs.blank?
+
+    cleaned_inputs = inputs.compact_blank
+    invalid = cleaned_inputs - VALID_INPUTS
+
+    errors.add(:units, "contain invalid values: #{invalid.join(', ')}") if invalid.any?
+  end
 
   # :nocov:
   def self.ransackable_attributes(_auth_object = nil)
