43c87c35dda50a7843fbf2604a8f15ec5e6be735
Michael Großklaus <mail@mgrossklaus.de>
2025-10-21 22:31:22 +0200

    Adds EventAttendees to active admin

---
+++ app/admin/event_attendees.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register EventAttendee do
+  menu parent: "Events"
+end
--- app/models/event_attendee.rb
+++ app/models/event_attendee.rb
@@ -6,5 +6,9 @@ class EventAttendee < ApplicationRecord
   def self.ransackable_attributes(_auth_object = nil)
     %w[created_at event_id id id_value updated_at user_id]
   end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[event user]
+  end
   # :nocov:
 end
