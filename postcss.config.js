e886d4ec7b9ff328608fff9136fb6585aefcca74
Michael Großklaus <mail@mgrossklaus.de>
2026-09-12 20:50:19 +0200

    Revert "Increase CSS coverage"
    This reverts commit 9c50d2c7191a483c1796ed088beac35879455b74.

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,10 @@
-export default (api) => {
-	const isActiveAdmin = api.file.basename == "active_admin.css";
-
-	return {
-		plugins: {
-			"@tailwindcss/postcss": {},
-			"postcss-import": {},
-			"postcss-preset-env": {
-				browsers: isActiveAdmin
-					? "baseline widely available"
-					: ">0.2%, defaults",
-			},
-			cssnano: {},
+export default {
+	plugins: {
+		"@tailwindcss/postcss": {},
+		"postcss-import": {},
+		"postcss-preset-env": {
+			browsers: "baseline widely available",
 		},
-	};
+		cssnano: {},
+	},
 };
