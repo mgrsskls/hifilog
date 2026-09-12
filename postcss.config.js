9c50d2c7191a483c1796ed088beac35879455b74
Michael Großklaus <mail@mgrossklaus.de>
2026-09-12 20:45:07 +0200

    Increase CSS coverage

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,16 @@
-export default {
-	plugins: {
-		"@tailwindcss/postcss": {},
-		"postcss-import": {},
-		"postcss-preset-env": {
-			browsers: "baseline widely available",
+export default (api) => {
+	const isActiveAdmin = api.file.basename == "active_admin.css";
+
+	return {
+		plugins: {
+			"@tailwindcss/postcss": {},
+			"postcss-import": {},
+			"postcss-preset-env": {
+				browsers: isActiveAdmin
+					? "baseline widely available"
+					: ">0.2%, defaults",
+			},
+			cssnano: {},
 		},
-		cssnano: {},
-	},
+	};
 };
