91b220cef5d45ee58745595c9b154c121e736236
Michael Großklaus <mail@mgrossklaus.de>
2026-09-26 13:55:26 +0200

    Do not throw a 500 if a wrong app_news ID is marked as read

--- app/controllers/app_news_controller.rb
+++ app/controllers/app_news_controller.rb
@@ -4,11 +4,13 @@ class AppNewsController < ApplicationController
   before_action :authenticate_user!
   skip_after_action :record_page_view
 
+  # permit keeps ids only as a list of values, so a missing or malformed ids marks nothing and the
+  # request redirects like a normal click. News the user has read already is skipped:
+  # app_news_users has a unique index, so a second click (for example from an older tab) would raise.
   def mark_as_read
-    params[:ids].each do |id|
-      news = AppNews.find(id.to_i)
-      current_user.app_news << news
-    end
+    ids = params.permit(ids: []).fetch(:ids, [])
+    unread = AppNews.where(id: ids).where.not(id: current_user.app_news_ids)
+    current_user.app_news << unread.to_a
 
     flash[:notice] = I18n.t(:generic_error_message) unless current_user.save
 
--- test/controllers/app_news_controller_test.rb
+++ test/controllers/app_news_controller_test.rb
@@ -18,4 +18,33 @@ class AppNewsControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
     assert_redirected_to dashboard_root_path
   end
+
+  test 'mark_as_read without ids marks nothing and redirects' do
+    sign_in users(:without_anything)
+
+    post app_news_mark_as_read_url
+
+    assert_redirected_to dashboard_root_path
+    assert_empty users(:without_anything).reload.app_news
+  end
+
+  test 'mark_as_read with one id instead of a list marks nothing and redirects' do
+    sign_in users(:without_anything)
+
+    post app_news_mark_as_read_url, params: { ids: app_news(:one).id }
+
+    assert_redirected_to dashboard_root_path
+    assert_empty users(:without_anything).reload.app_news
+  end
+
+  test 'mark_as_read skips news that is read already' do
+    user = users(:without_anything)
+    user.app_news << app_news(:one)
+    sign_in user
+
+    post app_news_mark_as_read_url(ids: [app_news(:one).id, app_news(:two).id])
+
+    assert_redirected_to dashboard_root_path
+    assert_equal [app_news(:one), app_news(:two)], user.reload.app_news.order(:id).to_a
+  end
 end
